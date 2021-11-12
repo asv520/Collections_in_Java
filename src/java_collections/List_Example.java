796c8b6a39b53f1d208376d15efd9194f296314a
asv520 <iamasv520@gmail.com>
2021-11-12 12:09:24 +0530

    Add 'List'

---
+++ src/java_collections/List_Example.java
@@ -0,0 +1,15 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class List_Example {
+    public static void main(String[] args) {
+//        List is an interface. ArrayList and LinkedList are its implementation classes.
+        List<Integer> list = new ArrayList<>();
+        List<String> other_list = new ArrayList<>();
+        List<Float> linked_list = new LinkedList<>();
+    }
+}
+//Refer "https://www.softwaretestinghelp.com/java-list-methods/" for list methods
